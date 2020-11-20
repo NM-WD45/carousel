280942b0fd47627cb73906503241619e6e42108c
N M <contact@nm-webdesign.fr>
2020-11-20 22:27:08 +0100

    Add files via upload

---
+++ carousel.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Entreprise | Kaina</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+
+        <!-- css -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="carousel.css"/>
+    </head>
+
+    <body id="body">
+ <div class="center">
+  <div class="wrapper">
+    <div class="inner">
+      <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
+        <div class="content">
+          <h1>Product Design</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+      <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
+        <div class="content">
+          <h1>Custom Website</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+      <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+      <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+      <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+      <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+            <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/1.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+            <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/2.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+            <div class="card">
+        <img src="https://colorlib.com/preview/theme/seogo/img/case_study/3.png">
+        <div class="content">
+          <h1>Digital Marketing</h1>
+          <h3>UI/UX, Design</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="map">
+    <button class="active first"></button>
+    <button class="second"></button>
+    <button class="third"></button>
+  </div>
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script>
+$('.toast').toast('show');
+</script>
+      
+      <!-- js -->
+        <script src="carousel.js"></script>
+    </body>
+</html>
